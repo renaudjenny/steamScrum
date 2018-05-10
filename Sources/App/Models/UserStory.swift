704ad4f7736b89bb55ca8604dc006dcfb599ab6e
Renaud Jenny <renaud.jenny@gmail.com>
2018-05-10 15:32:06 +0200

    [Evol] Begin work on Grooming Session (new Types)
    * Add structs like
        * Developer
        * GroomingSession
        * UserStory

---
+++ Sources/App/Models/UserStory.swift
@@ -0,0 +1,29 @@
+//
+//  UserStory.swift
+//  App
+//
+//  Created by Renaud JENNY on 10/05/2018.
+//
+
+import FluentPostgreSQL
+import Vapor
+
+struct UserStory: PostgreSQLModel {
+    var id: Int?
+    var estimatedPoints: [EstimatedPoint]?
+}
+
+// MARK: - Inner Types
+
+extension UserStory {
+    struct EstimatedPoint: Codable {
+        var date: Date
+        var points: Double
+    }
+}
+
+extension UserStory: Migration { }
+
+extension UserStory: Content { }
+
+extension UserStory: Parameter { }
