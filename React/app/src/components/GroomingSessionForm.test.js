415ef0f3216ffc80dd85cbe65dd3e2a0a11d43dd
Renaud Jenny <renaud.jenny@gmail.com>
2018-07-24 13:47:45 +0200

    [Test] Grooming Session Form - Add test on submit

--- React/app/src/components/GroomingSessionForm.test.js
+++ React/app/src/components/GroomingSessionForm.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { MemoryRouter } from 'react-router-dom'
 import GroomingSessionForm from './GroomingSessionForm';
+import axios from 'axios';
 import Enzyme from 'enzyme';
 import { mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
@@ -26,18 +27,33 @@ it('retrieve Grooming Sessions context', (done) => {
   );
   const groomingSessionForm = wrapper.find(GroomingSessionForm).instance();
 
-//  const mock = new MockAdapter(axios);
-//  const data = { groomingSessionsCount: 5, maximumGroomingSessionsCount: 98 };
-//  mock.onGet('/groomingSessionContext').reply(200, data);
-/*
+  const mock = new MockAdapter(axios);
+  const data = { groomingSessionsCount: 5, maximumGroomingSessionsCount: 98 };
+  mock.onGet('/groomingSessionsContext').reply(200, data);
+
   groomingSessionForm.refreshContext(() => {
     expect(groomingSessionForm.state.groomingSessionsCount).toEqual(data.groomingSessionsCount);
     expect(groomingSessionForm.state.maximumGroomingSessionsCount).toEqual(data.maximumGroomingSessionsCount);
     wrapper.unmount();
     done();
   });
-  */
+});
 
+it('post the form', (done) => {
+  const wrapper = mount(
+    <MemoryRouter>
+      <GroomingSessionForm />
+    </MemoryRouter>
+  );
+  const groomingSessionForm = wrapper.find(GroomingSessionForm).instance();
+
+  const mock = new MockAdapter(axios);
+  const data = { groomingSession: { id: '123', name: 'Posted Grooming Session', date: `${new Date()}` } };
+  mock.onPost('/groomingSessions').reply(201, data);
+
+  groomingSessionForm.submit(() => {
+    expect(groomingSessionForm.state.newGroomingSession).toEqual(data.groomingSession);
     wrapper.unmount();
     done();
+  });
 });
