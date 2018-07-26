9b99ae59f4213ae69069b4fa68429fc187118b3b
Renaud Jenny <renaud.jenny@gmail.com>
2018-07-26 14:10:26 +0200

    [Test] Grooming Session Form - Add test on text fields

--- Sources/App/Controllers/GroomingSessionController.swift
+++ Sources/App/Controllers/GroomingSessionController.swift
@@ -9,6 +9,8 @@ import Vapor
 
 final class GroomingSessionController {
 
+    static let maximumGroomingSessionsCount = 250
+
     func index(_ req: Request) throws -> Future<[GroomingSession]> {
         return GroomingSession.query(on: req).all()
     }
@@ -17,5 +19,23 @@ final class GroomingSessionController {
         return try req.content.decode(GroomingSession.self).flatMap(to: GroomingSession.self) { groomingSession in
             return groomingSession.save(on: req)
         }
+    }
+
+    func context(_ req: Request) throws -> Future<Context> {
+        return GroomingSession.query(on: req).count().map { (count) -> Context in
+            let context = Context(
+                groomingSessionsCount: count,
+                maximumGroomingSessionsCount: GroomingSessionController.maximumGroomingSessionsCount
+            )
+            return context
+        }
+    }
+}
+
+// MARK: - Inner types
+extension GroomingSessionController {
+    struct Context: Content {
+        var groomingSessionsCount: Int
+        var maximumGroomingSessionsCount: Int
     }
 }
