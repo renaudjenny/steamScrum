b7c80a743f6a7ed0e989fe5a49d7291d7a2f3b2d
Renaud Jenny <renaud.jenny@gmail.com>
2020-08-23 15:46:04 +0100

    fix(Grooming Session): tests after changing date format

---
+++ Sources/App/Extensions/Date+Formatter.swift
@@ -0,0 +1,12 @@
+import Foundation
+
+extension DateFormatter {
+    static let yyyyMMdd: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd"
+        formatter.calendar = Calendar(identifier: .iso8601)
+        formatter.timeZone = TimeZone(secondsFromGMT: 0)
+        formatter.locale = Locale(identifier: "en_US_POSIX")
+        return formatter
+    }()
+}
