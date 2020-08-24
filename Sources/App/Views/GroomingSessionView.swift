0d35a0a664b49f70824eb4a57a90bee7fdac686d
Renaud Jenny <renaud.jenny@gmail.com>
2020-08-24 22:33:02 +0100

    feat(Views): add a little bit of Style with Milligram

--- Sources/App/Views/GroomingSessionView.swift
+++ Sources/App/Views/GroomingSessionView.swift
@@ -5,6 +5,8 @@ struct GroomingSessionView {
         <html>
         <head>
         <meta charset="utf-8">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/milligram/1.4.1/milligram.min.css" integrity="sha512-xiunq9hpKsIcz42zt0o2vCo34xV0j6Ny8hgEylN3XBglZDtTZ2nwnqF/Z/TTCc18sGdvCjbFInNd++6q3J0N6g==" crossorigin="anonymous"
+        />
         \(script)
         <head>
         <body>
--- Sources/App/Views/Homepage.swift
+++ Sources/App/Views/Homepage.swift
@@ -6,42 +6,61 @@ struct Homepage {
         <html>
         <head>
         <meta charset="utf-8">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/milligram/1.4.1/milligram.min.css" integrity="sha512-xiunq9hpKsIcz42zt0o2vCo34xV0j6Ny8hgEylN3XBglZDtTZ2nwnqF/Z/TTCc18sGdvCjbFInNd++6q3J0N6g==" crossorigin="anonymous"
+        />
         \(script)
         <head>
         <body>
-        \(title)
-        \(groomingForm)
-        \(groomingSessionsList)
+            <div class="container">
+                \(title)
+                \(groomingForm)
+                \(groomingSessionsList)
+            </div>
         </body>
         </html>
         """
     )}
 
     var title: String { """
-        <h1>Steam Scrum</h1>
-        <p>This project changed a lot. It has been migrated to the last version of Vapor and will sooner be fully rendered in Swift! (See: <a href="https://github.com/swiftwasm/Tokamak">Tokamak project</a>)</p>
-        <p>This is ugly for now as it's pure HTML without CSS</p>
+        <div class="row">
+            <h1>Steam Scrum</h1>
+        </div>
+        <div class="row">
+            <p>This project changed a lot. It has been migrated to the last version of Vapor and will sooner be fully rendered in Swift! (See: <a href="https://github.com/swiftwasm/Tokamak">Tokamak project</a>)</p>
+        </div>
+        <div class="row">
+            <p>This is using pure Javascript (without any external libraries), and <a href="https://milligram.io">Milligram</a> to give a little bit of style here and there.</p>
+        </div>
+        <div class="row">
+            <p>The code is available here: <a href="https://github.com/renaudjenny/steamScrum">SteamScrum on GitHub</a></p>
+        </div>
         """
     }
 
     var groomingForm: String { """
-        <h2>Add a Grooming Session</h2>
-        <p>You can add grooming session to test the API</p>
+        <div class="row">
+            <h2>Add a Grooming Session</h2>
+        </div>
+        <div class="row">
+            <p>You can add grooming session to test the API</p>
+        </div>
         <form>
-        <div>
-        <label for="name">Grooming Session name: </label>
-        <input type="text" name="name" id="name" required>
-        </div>
-        <div>
-        <label for="date">Date of the session: </label>
-        <input type="date" name="date" id="date" required>
-        <button type="button" onclick="setGroomingSessionDateToNow()">Now</button>
-        </div>
-        <div>
-        <button type="button" onclick="createGroomingSession()">
-            Submit
-        </button>
-        </div>
+            <fieldset>
+                <label for="name">Grooming Session name: </label>
+                <input type="text" name="name" id="name" required>
+                <label for="date">Date of the session: </label>
+                <div class="row">
+                    <div class="column">
+                        <input type="date" name="date" id="date" required>
+                    </div>
+                    <div class="column">
+                        <button type="button" onclick="setGroomingSessionDateToNow()">Now</button>
+                    </div>
+                </div>
+                <button type="button" onclick="createGroomingSession()">
+                    Submit
+                </button>
+            </fieldset>
         </form>
         """
     }
@@ -51,10 +70,18 @@ struct Homepage {
         <p><strong>\(groomingSessionContext.groomingSessionsCount)/\(groomingSessionContext.maximumGroomingSessionsCount)</strong></p>
         \(groomingSessions.map({ groomingSession in
         """
-        <div>
-        <h3><a href="grooming_sessions/\(groomingSession.id!)">\(groomingSession.name)</a><h3>
-        <button type="button" onClick='removeGroomingSession("\(groomingSession.id!)")'>❌</button>
-        <h4>\(groomingSession.date.description)<h4>
+        <div class="row">
+            <div class="column">
+                <h3><a href="grooming_sessions/\(groomingSession.id!)">\(groomingSession.name)</a><h3>
+            </div>
+            <div class="column">
+                <button type="button" onClick='removeGroomingSession("\(groomingSession.id!)")'>❌</button>
+            </div>
+        </div>
+        <div class="row">
+            <div class="column">
+                <h4>\(groomingSession.date.description)<h4>
+            </div>
         </div>
         """
         }).joined(separator: "<br />")
