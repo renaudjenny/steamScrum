bbc6b91ad1ce3b86b2923b5dd8d46389580a536e
Renaud Jenny <renaud.jenny@gmail.com>
2020-08-26 21:06:41 +0100

    feat(Views): improve some style to Homepage

--- Sources/App/Views/Homepage.swift
+++ Sources/App/Views/Homepage.swift
@@ -25,52 +25,76 @@ struct Homepage {
     )}
 
     var title: String { """
-        <div class="row">
-            <h1>Steam Scrum</h1>
+        <div class="row" style="margin-top: 2em">
+            <div class="column">
+                <h1>Steam Scrum</h1>
+            </div>
         </div>
         <div class="row">
-            <p>This project changed a lot. It has been migrated to the last version of Vapor and will sooner be fully rendered in Swift! (See: <a href="https://github.com/swiftwasm/Tokamak">Tokamak project</a>)</p>
+            <div class="column">
+                <p>This project changed a lot. It has been migrated to the last version of Vapor and will sooner be fully rendered in Swift! (See: <a href="https://github.com/swiftwasm/Tokamak">Tokamak project</a>)</p>
+            </div>
         </div>
         <div class="row">
-            <p>This is using pure Javascript (without any external libraries), and <a href="https://milligram.io">Milligram</a> to give a little bit of style here and there.</p>
+            <div class="column">
+                <p>This is using pure Javascript (without any external libraries), and <a href="https://milligram.io">Milligram</a> to give a little bit of style here and there.</p>
+            </div>
         </div>
         <div class="row">
-            <p>The code is available here: <a href="https://github.com/renaudjenny/steamScrum">SteamScrum on GitHub</a></p>
+            <div class="column">
+                <p>The code is available here: <a href="https://github.com/renaudjenny/steamScrum">SteamScrum on GitHub</a></p>
+            </div>
         </div>
         """
     }
 
     var groomingForm: String { """
         <div class="row">
-            <h2>Add a Grooming Session</h2>
+            <div class="column">
+                <h2>Add a Grooming Session</h2>
+            </div>
         </div>
         <div class="row">
-            <p>You can add grooming session to test the API</p>
+            <div class="column">
+                <p>You can add grooming session to test the API</p>
+            </div>
         </div>
-        <form>
-            <fieldset>
-                <label for="name">Grooming Session name: </label>
-                <input type="text" name="name" id="name" required>
-                <label for="date">Date of the session: </label>
-                <div class="row">
-                    <div class="column">
-                        <input type="date" name="date" id="date" required>
-                    </div>
-                    <div class="column">
-                        <button type="button" onclick="setGroomingSessionDateToNow()">Now</button>
-                    </div>
-                </div>
-                <button type="button" onclick="createGroomingSession()">
-                    Submit
-                </button>
-            </fieldset>
-        </form>
+        <div class="row">
+            <div class="column">
+                <form>
+                    <fieldset>
+                        <label for="name">Grooming Session name: </label>
+                        <input type="text" name="name" id="name" required>
+                        <label for="date">Date of the session: </label>
+                        <div class="row">
+                            <div class="column">
+                                <input type="date" name="date" id="date" required>
+                            </div>
+                            <div class="column">
+                                <button type="button" onclick="setGroomingSessionDateToNow()">Now</button>
+                            </div>
+                        </div>
+                        <button type="button" onclick="createGroomingSession()">
+                            Submit
+                        </button>
+                    </fieldset>
+                </form>
+            </div>
+        </div>
         """
     }
 
     var groomingSessionsList: String { """
-        <h2>Grooming Sessions</h2>
-        <p><strong>\(groomingSessionContext.groomingSessionsCount)/\(groomingSessionContext.maximumGroomingSessionsCount)</strong></p>
+        <div class="row">
+            <div class="column">
+                <h2>Grooming Sessions</h2>
+            </div>
+        </div>
+        <div class="row">
+            <div class="column">
+                <p><strong>\(groomingSessionContext.groomingSessionsCount)/\(groomingSessionContext.maximumGroomingSessionsCount)</strong></p>
+            </div>
+        </div>
         \(groomingSessions.map({ groomingSession in
         """
         <div class="row">
