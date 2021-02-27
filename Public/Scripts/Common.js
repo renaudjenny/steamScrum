7304a09cb16724451e55163a1302846425679704
Renaud Jenny <renaud.jenny@gmail.com>
2021-02-27 16:41:05 +0000

    refactor(JavaScript): remove a useless null check

--- Public/Scripts/Common.js
+++ Public/Scripts/Common.js
@@ -1,8 +1,6 @@
 export const preventFormSubmit = (id, submitFunction) => {
     window.addEventListener("DOMContentLoaded", () => {
         const form = document.getElementById(id)
-        if (form == null) { return }
-
         form.addEventListener("submit", (event) => {
             event.preventDefault()
             submitFunction()
