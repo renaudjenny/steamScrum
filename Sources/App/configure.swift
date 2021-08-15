ed8510c61ab85e7b4a78cd9fad2d4ae8400a4922
Renaud Jenny <renaud.jenny@gmail.com>
2021-08-15 09:57:11 +0100

    fix(Configure): put back certificateVerification to .none for PSQL TLS

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -22,6 +22,7 @@ public func configure(_ app: Application) throws {
     } else if let databaseURL = Environment.get("DATABASE_URL"),
               var postgresConfig = PostgresConfiguration(url: databaseURL) {
         postgresConfig.tlsConfiguration = .makeClientConfiguration()
+        postgresConfig.tlsConfiguration?.certificateVerification = .none
         app.databases.use(.postgres(
             configuration: postgresConfig
         ), as: .psql)
