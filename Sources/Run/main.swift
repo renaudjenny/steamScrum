7ae46481d722ddfb5f24c158ac82f1ae081f680e
Renaud Jenny <renaud.jenny@gmail.com>
2025-10-04 12:32:11 +0200

    build(Packages): upgrade packages to the last version
    * Modernize `main` file to use async version of startings methods

--- Sources/Run/main.swift
+++ Sources/Run/main.swift
@@ -3,7 +3,11 @@ import Vapor
 
 var env = try Environment.detect()
 try LoggingSystem.bootstrap(from: &env)
-let app = Application(env)
-defer { app.shutdown() }
-try configure(app)
-try app.run()
+let app = try await Application.make(env)
+do {
+    try configure(app)
+    try await app.execute()
+} catch {
+    try await app.asyncShutdown()
+}
+try await app.asyncShutdown()
